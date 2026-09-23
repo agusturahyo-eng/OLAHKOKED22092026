9da280afec1272e43be5ea4d986efdacb422fa60
agusturahyo-eng <agusturahyo@gmail.com>
2026-09-24 03:43:59 +0700

    Update bikinkokedon.py

--- bikinkokedon.py
+++ bikinkokedon.py
@@ -305,13 +305,13 @@ def to_excel_bytes(df):
 st.title("⚡ Aplikasi Olah Data & Ekstrak PDF / ICONPRN")
 
 tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
-    "1️⃣ Tahap 1: Bikin Data", 
-    "2️⃣ Tahap 2: Hasil Koked",
-    "3️⃣ Tahap 3: Eksport Pdf PB",
-    "4️⃣ Tahap 4: Eksport PDF",
-    "5️⃣ Tahap 5: eksport ICONPRN",
-    "6️⃣ Tahap 6: Olah Data",
-    "7️⃣ Tahap 7: Info & Lokasi"
+    "1️⃣ 1: Bikin Data Untuk Koked", 
+    "2️⃣ 2: Hasil Koked",
+    "3️⃣ 3: Eksport Pdf PB",
+    "4️⃣ 4: Eksport PDF",
+    "5️⃣ 5: eksport ICONPRN",
+    "6️⃣ 6: Olah Data",
+    "7️⃣ 7: Info & Lokasi"
 ])
 
 # ==========================================
